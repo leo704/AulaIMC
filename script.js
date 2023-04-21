aa07a96732312007412ff51ce2e2529ac5b05ab8
unknown <d2021010746@unifei.edu.br>
2023-04-20 22:01:39 -0300

    Início do projeto

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log("Teste do JS");
+
+function calculaIMC() {
+  let peso, altura, resp, classificacao;
+  peso = parseFloat(document.getElementById("edtPeso").value);
+  altura = parseFloat(document.getElementById("edtAltura").value);
+  resp = peso / (altura * altura);
+
+    
+
+  if (resp <= 18.5) {
+    classificacao = " Abaixo do peso";
+  } else if (resp <= 24.9) {
+    classificacao = " Peso ideal";
+  } else if (peso <= 29.9) {
+    classificacao = " Levemente acima do peso";
+  } else {
+    classificacao = " programador, termine a tabela";
+  }
+
+  document.getElementById("resp").innerText ="O seu IMC é: " + resp +
+  "A sua classificação é a seguinte:" + classificacao;
+
+}

69b981f79955d67f3021c00e70776d10387ec1d2
leo704 <d2021010746@unifei.edu.br>
2023-04-27 18:42:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 console.log("Teste do JS");
 
 function calculaIMC() {
-  let peso, altura, resp, classificacao;
-  peso = parseFloat(document.getElementById("edtPeso").value);
-  altura = parseFloat(document.getElementById("edtAltura").value);
-  resp = peso / (altura * altura);
+    let peso, altura, resp, classificacao;
+    peso = parseFloat(document.getElementById("edtPeso").value);
+    altura = parseFloat(document.getElementById("edtAltura").value);
+    resp = peso / (altura * altura);
 
-    
+    if (resp <= 18.5) {
+        classificacao = " Abaixo do peso";
+    } else if (resp <= 24.9) {
+        classificacao = " Peso ideal";
+    } else if (peso <= 29.9) {
+        classificacao = " Levemente acima do peso";
+    } else {
+        classificacao = " programador, termine a tabela";
+    }
 
-  if (resp <= 18.5) {
-    classificacao = " Abaixo do peso";
-  } else if (resp <= 24.9) {
-    classificacao = " Peso ideal";
-  } else if (peso <= 29.9) {
-    classificacao = " Levemente acima do peso";
-  } else {
-    classificacao = " programador, termine a tabela";
-  }
+    if (resp) {
+        document.getElementById("resp").innerText = "O seu IMC é: " + resp +
+            "A sua classificação é a seguinte:" + classificacao;
+    }
 
-  document.getElementById("resp").innerText ="O seu IMC é: " + resp +
-  "A sua classificação é a seguinte:" + classificacao;
+
 
 }
